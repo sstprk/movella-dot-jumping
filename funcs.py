3bf35b15f8261b2e226346a24057732c495d39a2
sstprk <sstprk.bs@gmail.com>
2023-09-01 18:57:19 +0300

    integration and filter trials

--- funcs.py
+++ funcs.py
@@ -1,14 +1,17 @@
 #Salih Toprak
 import numpy as np
 from scipy.integrate import cumtrapz, quad
+import scipy.signal as signal
 import math as m
 
 class functions:
     def __init__(self):
         pass
 
-    def accFilter(acc):
-        pass
+    def accFilter(acc, freq, Fs=20):
+        [b,a]=signal.butter(2,Fs/freq,'low'); 
+        ac=signal.lfilter(b,a,acc);
+        return ac
 
     def accTodist(a, t):
         def func(x, ac):
@@ -22,8 +25,13 @@ class functions:
             t1 = (i-1)/60
             t2 = i/60      
             v = quad(func, t1, t2, args=(a[i]))
-            Vo += v[1]
+            Vo += v[0]
             p = quad(func, t1, t2, args=(Vo))
-            Po += p[1]
-            location.append(float(Po))
+            location.append(float(p[0]))
         return location
+    
+    def timeAxis(frame_count, freq):
+        timeAx = []
+        for i in range(frame_count):
+            timeAx.append(i/freq)
+        return timeAx
--- main.py
+++ main.py
@@ -5,38 +5,43 @@ from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.animation as animation
 import pandas as pd
 
-path = "C:\\Users\\Salih\\Desktop\\Data\\XDOT\\Sali\\DenemeFreeAcc2\\Deneme2.xlsx"
+path = "C:\\Users\\Salih\\Desktop\\Data\\XDOT\\Sali\\Swing1\\Swing1.xlsx"
 
 df = pd.DataFrame(pd.read_excel(path))
 
-acc_x = list(df.loc[:,"FreeAcc_X"])
-acc_y = list(df.loc[:,"FreeAcc_Y"])
-acc_z = list(df.loc[:,"FreeAcc_Z"])
+rawAcc_x = list(df.loc[:,"FreeAcc_X"])
+rawAcc_y = list(df.loc[:,"FreeAcc_Y"])
+rawAcc_z = list(df.loc[:,"FreeAcc_Z"])
 
-locationX = functions.accTodist(acc_x, len(acc_x))
-locationY = functions.accTodist(acc_y, len(acc_y))
-locationZ = functions.accTodist(acc_z, len(acc_z))
+filteredAccX = functions.accFilter(rawAcc_x, 60)
+filteredAccY = functions.accFilter(rawAcc_y, 60)
+filteredAccZ = functions.accFilter(rawAcc_z, 60)
 
+locationX = functions.accTodist(filteredAccX, len(filteredAccX))
+locationY = functions.accTodist(filteredAccY, len(filteredAccY))
+locationZ = functions.accTodist(filteredAccZ, len(filteredAccZ))
+
+timeAxis = functions.timeAxis(len(filteredAccY), 60)
 plt.figure(1)
 
 plt.subplot(2,3,1)
 plt.title("Pos X")
-plt.plot(locationX, color="green")
+plt.plot(timeAxis, locationX , color="green")
 plt.subplot(2,3,2)
 plt.title("Pos Y")
-plt.plot(locationY, color="blue")
+plt.plot(timeAxis, locationY, color="blue")
 plt.subplot(2,3,3)
 plt.title("Pos Z")
-plt.plot(locationZ, color="red")
+plt.plot(timeAxis, locationZ, color="red")
 plt.subplot(2,3,4)
 plt.title("Acc X")
-plt.plot(acc_x, color="green")
+plt.plot(timeAxis, filteredAccX, color="green")
 plt.subplot(2,3,5)
 plt.title("Acc Y")
-plt.plot(acc_y, color="blue")
+plt.plot(timeAxis, filteredAccY, color="blue")
 plt.subplot(2,3,6)
 plt.title("Acc Z")
-plt.plot(acc_z, color="red")
+plt.plot(timeAxis, filteredAccZ, color="red")
 
 fig = plt.figure(2)
 ax = fig.add_subplot(111, projection="3d")
