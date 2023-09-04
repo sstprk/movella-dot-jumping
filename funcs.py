1b508688d5c5456d69bc3c3d3a51d53eb6e1f03d
sstprk <sstprk.bs@gmail.com>
2023-09-04 18:03:43 +0300

    filtering

--- funcs.py
+++ funcs.py
@@ -1,34 +1,52 @@
 #Salih Toprak
 import numpy as np
-from scipy.integrate import cumtrapz, quad
+from scipy.integrate import quad
 import scipy.signal as signal
 import math as m
+import pandas as pd
 
 class functions:
-    def __init__(self):
-        pass
+    def __init__(self, path):
+        self.path = path
+        self.df = pd.DataFrame(pd.read_excel(self.path))
 
-    def accFilter(acc, freq, Fs=20):
-        [b,a]=signal.butter(2,Fs/freq,'low'); 
-        ac=signal.lfilter(b,a,acc);
+        self.rawAcc_x = list(self.df.loc[:,"FreeAcc_X"])
+        self.rawAcc_y = list(self.df.loc[:,"FreeAcc_Y"])
+        self.rawAcc_z = list(self.df.loc[:,"FreeAcc_Z"])
+
+        self.filteredAccX = functions.accFilter(self.rawAcc_x)
+        self.filteredAccY = functions.accFilter(self.rawAcc_y)
+        self.filteredAccZ = functions.accFilter(self.rawAcc_z)
+
+        self.locationX = functions.accTodist(self.filteredAccX, len(self.filteredAccX))
+        self.locationY = functions.accTodist(self.filteredAccY, len(self.filteredAccY))
+        self.locationZ = functions.accTodist(self.filteredAccZ, len(self.filteredAccZ))
+
+        self.timeA = functions.timeAxis(len(self.filteredAccY), 60)
+
+    def accFilter(acc):
+        fs = 30.0 
+        cutoff = 2
+        nyq = 0.5 * fs  
+        order = 2
+        normal_cutoff = cutoff/nyq
+        y = signal.medfilt(acc, 21)
+        b, a = signal.butter(order, normal_cutoff, btype="low", analog=False)
+        ac = signal.filtfilt(b, a, y)
         return ac
 
     def accTodist(a, t):
         def func(x, ac):
             return ac
-        loc = 0
-        location = []
-        Vo = 0
-        Po = 0
+        position = []
         i = 1
         for i in range(t):  
             t1 = (i-1)/60
             t2 = i/60      
-            v = quad(func, t1, t2, args=(a[i]))
-            Vo += v[0]
-            p = quad(func, t1, t2, args=(Vo))
-            location.append(float(p[0]))
-        return location
+            v = quad(func, t1, t2, args=(a[i]-0.55))
+            p = quad(func, t1, t2, args=(v[0]))
+            position.append(float(p[0]))
+        return position
     
     def timeAxis(frame_count, freq):
         timeAx = []
--- main.py
+++ main.py
@@ -1,51 +1,51 @@
 #Salih Toprak
-from funcs import functions
+import funcs
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.animation as animation
 import pandas as pd
 
-path = "C:\\Users\\Salih\\Desktop\\Data\\XDOT\\Sali\\Swing1\\Swing1.xlsx"
+#funcs.py
+cls = funcs.functions("C:\\Users\\Salih\\Desktop\\Data\\XDOT\\Sali\\Swing1\\Swing1.xlsx")
 
-df = pd.DataFrame(pd.read_excel(path))
-
-rawAcc_x = list(df.loc[:,"FreeAcc_X"])
-rawAcc_y = list(df.loc[:,"FreeAcc_Y"])
-rawAcc_z = list(df.loc[:,"FreeAcc_Z"])
-
-filteredAccX = functions.accFilter(rawAcc_x, 60)
-filteredAccY = functions.accFilter(rawAcc_y, 60)
-filteredAccZ = functions.accFilter(rawAcc_z, 60)
-
-locationX = functions.accTodist(filteredAccX, len(filteredAccX))
-locationY = functions.accTodist(filteredAccY, len(filteredAccY))
-locationZ = functions.accTodist(filteredAccZ, len(filteredAccZ))
-
-timeAxis = functions.timeAxis(len(filteredAccY), 60)
+#Figure for comparison between raw and filtered data
 plt.figure(1)
 
 plt.subplot(2,3,1)
-plt.title("Pos X")
-plt.plot(timeAxis, locationX , color="green")
+plt.title("rAcc X")
+plt.plot(cls.timeA, cls.rawAcc_x, color="green")
 plt.subplot(2,3,2)
-plt.title("Pos Y")
-plt.plot(timeAxis, locationY, color="blue")
+plt.title("rAcc Y")
+plt.plot(cls.timeA, cls.rawAcc_y, color="blue")
 plt.subplot(2,3,3)
-plt.title("Pos Z")
-plt.plot(timeAxis, locationZ, color="red")
+plt.title("rAcc Z")
+plt.plot(cls.timeA, cls.rawAcc_z, color="red")
 plt.subplot(2,3,4)
-plt.title("Acc X")
-plt.plot(timeAxis, filteredAccX, color="green")
+plt.title("fAcc X")
+plt.plot(cls.timeA, cls.filteredAccX, color="green")
 plt.subplot(2,3,5)
-plt.title("Acc Y")
-plt.plot(timeAxis, filteredAccY, color="blue")
+plt.title("fAcc Y")
+plt.plot(cls.timeA, cls.filteredAccY, color="blue")
 plt.subplot(2,3,6)
-plt.title("Acc Z")
-plt.plot(timeAxis, filteredAccZ, color="red")
+plt.title("fAcc Z")
+plt.plot(cls.timeA, cls.filteredAccZ, color="red")
 
-fig = plt.figure(2)
+#Figure for position on each axis
+plt.figure(2)
+
+plt.subplot(1,3,1)
+plt.plot(cls.timeA, cls.locationX , color="green")
+plt.subplot(1,3,2)
+plt.plot(cls.timeA, cls.locationY, color="blue")
+plt.subplot(1,3,3)
+plt.plot(cls.timeA, cls.locationZ, color="red")
+
+#Figure for 3D displacement
+fig = plt.figure(3)
+
 ax = fig.add_subplot(111, projection="3d")
-ax.plot(locationX, locationY, locationZ, color="black")
+ax.plot(cls.locationX, cls.locationY, cls.locationZ, color="black")
+
 plt.show()
 
 #Deneme1 = 33.5 cm
