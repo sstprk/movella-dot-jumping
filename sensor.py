1ac0c39b00ad9b5cdab6090ca59ecd0022e1e300
sstprk <sstprk.bs@gmail.com>
2023-09-28 19:56:57 +0300

    trying to implement working matlab code

--- sensor.py
+++ sensor.py
@@ -9,8 +9,8 @@ import pandas as pd
 
 import math as m
 
-from pykalman import KalmanFilter
-#from kalman import KalmanFilter
+#from pykalman import KalmanFilter
+from kalman import KalmanFilter
 
 class Sensor:
     sensor_count = 0
@@ -35,85 +35,112 @@ class Sensor:
         self.frame_array = list(range(len(self.rawAcc[0])))
         self.newTime = []
         self.timeA = []
-        
-        self.timeAxis()
 
-        self.filteredAcc = self.filter(self.rawAcc)
-        self.rawVelocity = self.integrate(self.filteredAcc)
-        self.filteredVel = self.filter(self.rawVelocity)
-        self.rawPosition = self.integrate(self.filteredVel)
-        self.filteredPos = self.filter(self.rawPosition)
+        self.filteredAcc = []
+        self.Velocity = []
+        self.Position = []
+
+        self.organise()
+        self.timeAxis()
+        self.filter()
         
         Sensor.sensor_count += 1
 
-    def filter(self, rawD):
-        fs = self.freq 
-        cutoff = 2
-        nyq = 0.5 * fs  
-        order = 2
-        normal_cutoff = cutoff/nyq
-        final = []
+    def filter(self):
+        self.rawAcc = np.asmatrix(self.rawAcc)
+        mag = [np.multiply(self.rawAcc[:,0], self.rawAcc[:,0])+
+               np.multiply(self.rawAcc[:,1], self.rawAcc[:,1])+
+               np.multiply(self.rawAcc[:,2], self.rawAcc[:,2])]
+        accMag = np.sqrt(mag)
 
-        for data in rawD:
-            dt = 0.01
-            F = np.array([[1, dt, 0.5*dt**2], [0, 1, dt], [0, 0, 1]])
-            H = np.array([0, 0, 1]).reshape(1, 3)
-            Q = np.array([[0.2, 0.0, 0.0], [0.0, 0.1, 0.0], [0.0, 0.0, 10e-4]])
-            R = 0.0020
-            P0 = np.array([[0, 0, 0], [0, 0, 0], [0, 0, R]])
-            X0 = np.array([0, 0, data[0]])
+        samplePeriod = 1/256
+        filtCutOff = 0.001
+        b, a = signal.butter(1, (2*filtCutOff)/(1/samplePeriod), "high")
+        magFilted = signal.filtfilt(b, a, np.ravel(accMag))
+        magFilted = abs(magFilted)
 
-            kf = KalmanFilter(transition_matrices=F, observation_matrices=H, transition_covariance=Q, observation_covariance=R, initial_state_mean=X0, initial_state_covariance=P0)
-            predictions = []
-            n_timesteps = len(data)
-            n_dim_state = 3
-            filtered_state_means = np.zeros((n_timesteps, n_dim_state))
-            filtered_state_covariances = np.zeros((n_timesteps, n_dim_state, n_dim_state))
-            
-            for t in range(n_timesteps):
-                if t == 0:
-                    filtered_state_means[t] = X0
-                    filtered_state_covariances[t] = P0
-                else:
-                    filtered_state_means[t], filtered_state_covariances[t] = kf.filter_update()
+        filtCutOff = 5
+        b, a = signal.butter(1,(2*filtCutOff)/(1/samplePeriod), "low")
+        magFilted = signal.filtfilt(b, a, magFilted)
+        
+        stationary = magFilted < 0.05
+        stationary = stationary.astype(int)
 
-            #y = signal.medfilt(predictions, 3)
-            #b, a = signal.butter(order, normal_cutoff, btype="low")
-            #ac = signal.filtfilt(b, a, predictions)
+        self.rawAcc = self.rawAcc * 9.81
+        t=1
 
-            final.append(predictions)
-        return final
+        velocity = np.zeros(shape=(len(self.rawAcc), 3))
+
+        for t1 in range(len(velocity)):
+
+            #acInterspace = [accFilted[t1], accFilted[t1-1]] 
+            velocity[t1,:] = velocity[t1-1,:] + self.rawAcc[t,:] * samplePeriod
+            for ix in range(2):
+                if stationary[t1] == 1:
+                    velocity[t1] = [0, 0, 0]
+        print(velocity)
+        self.Velocity = velocity
+        velDrift = np.zeros(np.shape(velocity))
+        print(velDrift)
+        stationary_start = np.where(np.diff(stationary) == -1)
+        stationary_start = np.insert(stationary_start, 0, 0)
+        stationary_end = np.where(np.diff(stationary) == 1)
+        stationary_end = np.insert(stationary_end, 0, 0)
+
+        for i in range(np.size(stationary_end)):
+            drift_rate = velocity[stationary_end[i], :]/ (stationary_end[i] - stationary_start[i])
+
+            enum = np.array(range(1, stationary_end[i] - stationary_start[i]))
+
+            drift = [np.transpose(enum*drift_rate[0]), np.transpose(enum*drift_rate[1]), np.transpose(enum*drift_rate[2])]
+
+            velDrift[stationary_start[i]:stationary_end[i]-1, :] = drift
+
+        velocity = velocity - velDrift
+
+        pos = np.zeros(shape=(len(velocity), 3))
+        for t2 in range(np.size(pos)):
+            #velInterspace = [velocity[t2], velocity[t2-1]]
+            pos[t2,:] = pos[t2-1,:] + velocity[t2,:]*samplePeriod
+
+        self.Position.append(pos)
 
     def integrate(self, filtD):
-        #func = lambda x, ac: ac
         results = []
         for data in filtD:
             tempResult = []
             rSum = 0
             i = 1
+            tempResult = cumtrapz(data, initial=0)
 
-            for i in range(len(data)):  
+            """for i in range(len(data)):  
                 t1 = (i-1)/60
                 t2 = i/60
 
-                """v = quad(func, t2, t1, args=(a[i]))
+                v = quad(func, t2, t1, args=(a[i]))
                 Vx = v[1] - v[0]
                 Vsum += Vx
                 tempV.append(float(v[0]))
 
                 p = quad(func, t2, t1, args=(v[0]))
                 Psum += p[0]
-                tempP.append(float(p[0]))"""
+                tempP.append(float(p[0]))
 
                 interspaceData = [data[i-1], data[i]]
 
                 res = cumtrapz(interspaceData, initial=0)
                 r = res[1]
                 rSum += r
-                tempResult.append(r)
+                tempResult.append(rSum)"""
             results.append(tempResult)
         return results
     
     def timeAxis(self):
         for i in range(self.frame_count):
             self.timeA.append(i/self.freq)
+
+    def organise(self):
+        rawTemp = np.zeros(shape=(self.frame_count, 3))
+        for t in range(self.frame_count):
+            rawTemp[t] = [self.rawAcc[0][t], self.rawAcc[1][t], self.rawAcc[2][t]]
+        self.rawAcc = rawTemp
